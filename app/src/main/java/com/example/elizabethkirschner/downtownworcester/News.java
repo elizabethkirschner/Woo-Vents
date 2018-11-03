83cb897d56f1d74c6db4730bfddc62cafc08e4ff
Elizabeth Kirschner <eekirschner@wpi.edu>
2018-11-03 13:06:12 -0400

    all pages

---
+++ app/src/main/java/com/example/elizabethkirschner/downtownworcester/News.java
@@ -0,0 +1,47 @@
+package com.example.elizabethkirschner.downtownworcester;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class News extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_events:
+                    mTextMessage.setText(R.string.title_events);
+                    return true;
+                case R.id.navigation_discover:
+                    mTextMessage.setText(R.string.title_discover);
+                    return true;
+                case R.id.navigation_news:
+                    mTextMessage.setText(R.string.title_news);
+                    return true;
+                case R.id.navigation_transportation:
+                    mTextMessage.setText(R.string.title_transportation);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
