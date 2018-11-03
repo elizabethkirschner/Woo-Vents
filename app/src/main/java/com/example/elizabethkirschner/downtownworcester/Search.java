50fb2dbff574ed3805560e5ab2bf15da38cbd4c0
Elizabeth Kirschner <eekirschner@wpi.edu>
2018-11-03 12:36:07 -0400

    error fix

--- app/src/main/java/com/example/elizabethkirschner/downtownworcester/Search.java
+++ app/src/main/java/com/example/elizabethkirschner/downtownworcester/Search.java
@@ -17,14 +17,14 @@ public class Search extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_events:
+                    mTextMessage.setText(R.string.title_events);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_discover:
+                    mTextMessage.setText(R.string.title_discover);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_news:
+                    mTextMessage.setText(R.string.title_news);
                     return true;
             }
             return false;
